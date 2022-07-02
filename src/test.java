51d0ead827ad240a96c9a08e23a7e851a25eb995
JE <jemh62korea@gmail.com>
2022-07-02 12:40:34 +0900

    Initial commit

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args){
+        System.out.println("Hello Github");
+        System.out.println("nice to meet you");
+        System.out.println("Thank you!");
+
+
+    }
+}
